0d34217e4e5f21f97351b3f3d77a91f7440ce8a5
Juan I. Chiappella <jichiappella@itsynch.com>
2025-10-08 21:48:25 -0300

    La semilla de la vida

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    keycloak: {
+        url:  'http://localhost:8080' ,//https://dacs2025/secure
+        realm: 'dacs',
+        clientId: 'dacs-fe'
+    },
+      backendForFrontendUrl: '/bff'
+};
